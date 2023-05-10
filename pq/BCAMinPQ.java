2c11d6b7e8574b301a799768c543a56e2c94f499
Julia Thompson <jewelst2014@gmail.com>
2023-05-10 09:35:39 -0400

    Fixed pushDown!!!

--- pq/BCAMinPQ.java
+++ pq/BCAMinPQ.java
@@ -1,13 +1,13 @@
 package pq;
+
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
 
-  /*Stores items in heap starting at index 1*/
+  /* Stores items in heap starting at index 1 */
   private ArrayList<E> heap = new ArrayList<>();
 
-  
   public BCAMinPQ() {
     /* dummy element in index 0, making actual elements start at index 1 */
     heap.add(null);
@@ -16,30 +16,32 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
   /**
    * Return index of the left child of element at index i.
    */
-  private int leftChildOf(int i){
-    if (2 * 1 > heap.size() -1) return -1;
+  private int leftChildOf(int i) {
+    if (2 * i > heap.size() - 1)
+      return -1;
     return 2 * i;
   }
 
   /**
    * Return index of the right child of element at index i.
    */
-  private int rightChildOf(int i){
-    if (2 * i + 1 > heap.size() - 1) return -1;
+  private int rightChildOf(int i) {
+    if (2 * i + 1 > heap.size() - 1)
+      return -1;
     return 2 * i + 1;
   }
 
   /**
    * Return index of the parent of element at index i.
    */
-  private int parentOf(int i){
+  private int parentOf(int i) {
     return i / 2;
   }
 
   /**
    * Swap the elements at indexes i1 and i2.
    */
-  private void swap(int i1, int i2){
+  private void swap(int i1, int i2) {
     E temp = heap.get(i1);
     heap.set(i1, heap.get(i2));
     heap.set(i2, temp);
@@ -64,23 +66,22 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
   }
 
   /** Adds a new element to the the queue. */
-  public void enqueue(E o){
+  public void enqueue(E o) {
     heap.add(o);
-    pushUp(heap.size()-1);
+    pushUp(heap.size() - 1);
   }
 
-  /* Try BCAMinPQTest now! You should pass 0.1 to 0.6*/
-
+  /* Try BCAMinPQTest now! You should pass 0.1 to 0.6 */
 
   /**
    * Returns the next item from the queue without popping it.
    * If no item, returns null
    */
-  public E peek(){
+  public E peek() {
     return heap.get(1);
   }
 
-  /* Try BCAMinPQTest now! You should pass 1a*/
+  /* Try BCAMinPQTest now! You should pass 1a */
 
   /**
    * Push down the element at index i (swapping with its smallest child)
@@ -89,14 +90,18 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
    * is the only element out of place.
    */
   private void pushDown(int i) {
-    E curr = heap.get(i);
-    if (curr.compareTo(heap.get(leftChildOf(i))) < 0 || curr.compareTo(heap.get(rightChildOf(i))) < 0) {
+    if (size() <= 1 || (leftChildOf(i) == -1 && rightChildOf(i) == -1)) {
       return;
     }
-    if (heap.get(leftChildOf(i)).compareTo(heap.get(rightChildOf(i))) < 0) {
+    E curr = heap.get(i);
+    int left = curr.compareTo(heap.get(leftChildOf(i)));
+    if ((rightChildOf(i) == -1 || curr.compareTo(heap.get(rightChildOf(i))) <= 0) && left <= 0) {
+      return;
+    }
+    if (rightChildOf(i) == -1 || heap.get(leftChildOf(i)).compareTo(heap.get(rightChildOf(i))) < 0) {
       swap(i, leftChildOf(i));
       pushDown(leftChildOf(i));
-    } else {
+    } else  {
       swap(i, rightChildOf(i));
       pushDown(rightChildOf(i));
     }
@@ -106,9 +111,9 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
    * Removes the smallest item from the queue and returns it.
    *
    * @exception NoSuchElementException
-   *                if the queue is empty.
+   *                                   if the queue is empty.
    */
-  public E dequeue(){
+  public E dequeue() {
 
     if (isEmpty())
       throw new NoSuchElementException("MinPQ is empty");
@@ -119,12 +124,15 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
     return ret;
   }
 
-  /* Try BCAMinPQTest now! You should pass the remaining tests 1b through 8, and HeapSort*/
+  /*
+   * Try BCAMinPQTest now! You should pass the remaining tests 1b through 8, and
+   * HeapSort
+   */
 
   /**
    * Returns whether the queue is empty or not.
    */
-  public boolean isEmpty(){
+  public boolean isEmpty() {
     return size() == 0;
   }
 
@@ -132,7 +140,7 @@ public class BCAMinPQ<E extends Comparable<E>> implements BCAQueue<E> {
    * Returns the number of items currently in the queue.
    */
   public int size() {
-    return heap.size()-1;  // The arraylist has an extra element at position 0, hence the subtraction of 1.
+    return heap.size() - 1; // The arraylist has an extra element at position 0, hence the subtraction of 1.
   }
 
   /**
